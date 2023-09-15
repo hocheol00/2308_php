5b236ef809c239e9922890ddaf987d1927874c9e
hocheol00 <ghcjf0702@naver.com>
2023-09-15 11:35:43 +0900

    230915php

--- php/Tng/88_02_rsp.php
+++ php/Tng/88_02_rsp.php
@@ -2,21 +2,53 @@
 
 // $in_str = fgets(STDIN);
 
+while(true) {
+
 $game = rand(1,3);
+echo "가위는 's', 바위는 'r', 보는 'p' 실행종료는 'e' 입니다";
+$input = "";
+$input = trim(fgets(STDIN));
 
-$input = "가위";
-if(in)
-		if ($input == "가위" && $game == 1) {
-			echo "무승부";
-		}
-		else if ($input == "가위" && $game == 2) {
-			echo "패배 ";
-		}
-		else if ($input == "가위" && $game == 3) {
-			echo "승리";
-		}
-	 
-			
 
-	
+if($input == "s") {
+		if ($game == 1) {
+			echo "무승부\n";
+		}
+		else if ($game == 2) {
+			echo "패배\n";
+		}
+		else if ($game == 3) {
+			echo "승리\n";
+		}
+	}
+
+else if($input == "r") {
+	if ($game == 1) {
+		echo "승리\n";
+	}
+	else if ($game == 2) {
+		echo "무승부\n";
+	}
+	else if ($game == 3) {
+		echo "패배\n";
+	}
+}
+
+else if ($input == "p") {
+	if ($game == 1) {
+		echo "패배\n";
+	}
+	else if ($game == 2) {
+		echo "승리\n";
+	}
+	else if ($game == 3) {
+		echo "무승부\n";
+	}
+}
+
+else if ($input =="e") {
+	break;
+}
+
+}
 
