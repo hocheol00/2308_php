a54ed66a4ad3385ce57bb6d846395ffa2ed26ffa
hocheol00 <ghcjf0702@naver.com>
2023-10-23 23:43:18 +0900

    1023 js

--- javascript/js/04.js
+++ javascript/js/04.js
@@ -19,3 +19,8 @@ console.log(u_name);
 //const : 중복 선언 불가능, 재할당 불가능, 블록레벨 스코프
 const AGE = 19;
 console.log(AGE);
+
+let num1 = "15";
+let num2 = 5;
+console.log(parseInt(num1) + num2); // parsint : 문자열을 숫자로 형변환
+
