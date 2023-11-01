52cb16154900dcede2fa6aa0a7281cf95361baab
hocheol00 <ghcjf0702@naver.com>
2023-11-02 01:03:30 +0900

    1102-crud

--- CRUD/src/php/insert.php
+++ CRUD/src/php/insert.php
@@ -11,8 +11,25 @@ require_once(ROOT."../lib/lib.php"); // db관련 라이브러리
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
+	<link rel="stylesheet" href="../css/common.css">
+	
 </head>
 <body>
-	<h1>aa</h1>
+	<div class="main">
+		<div class="header">TO-DO-LIST</div>
+		<div class="main_insert">
+			<form action="">
+				<input type="date" value="xxx" min="yyy" max="zzz">
+				<input type="text" id="id" placeholder="제목">
+				<textarea name="" id="" placeholder="오늘의 내용"></textarea>
+				<br>
+				<div class="grid">
+					<button type="submit">작성</button>
+					<button class="insert-delete" onclick="location.href=/src/php/list.php">이 전</button>
+					<!-- <a class="insert-delete" href="/src/php/list.php">이전</a> -->
+				</div>
+			</form>
+		</div>
+	</div>
 </body>
 </html>
