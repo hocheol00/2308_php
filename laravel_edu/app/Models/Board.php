6210df4b45f153d8856aac3851513f2be8e79ca4
hocheol00 <ghcjf0702@naver.com>
2023-11-13 17:26:40 +0900

    1113 - 수업

--- laravel_edu/app/Models/Board.php
+++ laravel_edu/app/Models/Board.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Board extends Model
 {
     use HasFactory;
+    // 모델 생성 : php artisan make:model 모델명 -mfs
+    // [-mfs] 옵션으로 migration, factory, seeder를 한번에 생산
 }
